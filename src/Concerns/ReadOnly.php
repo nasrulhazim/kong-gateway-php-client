2decd0bda5e66164420f1c70214c6e03361f3287
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2021-06-18 09:12:55 +0800

    Added ReadOnly Trait

---
+++ src/Concerns/ReadOnly.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace KongGateway\Concerns;
+
+trait ReadOnly
+{
+	public function store($data)
+    {
+        throw new \Exceptions('API endpoint did not exist');
+    }
+
+    public function update($identifier, $data)
+    {
+        throw new \Exceptions('API endpoint did not exist');
+    }
+
+    public function delete($identifier)
+    {
+        throw new \Exceptions('API endpoint did not exist');
+    }
+}
