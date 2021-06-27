c2ce133835ad4dbcc168e7a67b076be354dfda43
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2021-06-28 07:34:58 +0800

    Added Laravel Service Provider

---
+++ src/Console/Commands/PublishCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CleaniqueCoders\Profile\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PublishCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'kong:publish';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish Kong Gateway PHP Client configuration';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->call('vendor:publish', [
+            '--tag' => 'kong-config'
+        ]);
+    }
+}
---
+++ src/KongGatewayServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace KongGateway;
+
+use Illuminate\Support\ServiceProvider;
+
+class KongGatewayServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Configuration
+         */
+        $this->publishes([
+            __DIR__ . '/../config/kong.php' => config_path('kong.php'),
+        ], 'kong-config');
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/kong.php', 'kong-config'
+        );
+
+        /*
+         * Commands
+         */
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                // @todo
+                // \KongGateway\Console\Commands\TestConnectionCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+    }
+}
