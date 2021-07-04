12ccbdd379553aa0456cad5f780793399d785b09
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2021-07-04 13:28:32 +0800

    JWT

--- src/AdminAPI/Plugin/Authentication/JWT.php
+++ src/AdminAPI/Plugin/Authentication/JWT.php
@@ -8,6 +8,11 @@ class JWT extends Plugin
 {
     public $path = 'jwts';
 
+    /**
+     * Create a JWT credential
+     * 
+     * @link https://docs.konghq.com/hub/kong-inc/jwt/#create-a-jwt-credential
+     */
     public function createCredential($consumer)
     {
         return $this->response(
@@ -15,6 +20,23 @@ class JWT extends Plugin
         );
     }
 
+    /**
+     * Retrieve the Consumer associated with a JWT
+     * 
+     * @link https://docs.konghq.com/hub/kong-inc/jwt/#retrieve-the-consumer-associated-with-a-jwt
+     */
+    public function show($identifier)
+    {
+        return $this->response(
+            $this->client()->get($this->pluginPath().'/'.$identifier.'/consumer')
+        );
+    }
+
+    /**
+     * List JWT credentials
+     * 
+     * @link https://docs.konghq.com/hub/kong-inc/jwt/#list-jwt-credentials
+     */
     public function showConsumerCredentials($consumer)
     {
         return $this->response(
@@ -22,6 +44,11 @@ class JWT extends Plugin
         );
     }
 
+    /**
+     * Delete a JWT Credential
+     * 
+     * @link https://docs.konghq.com/hub/kong-inc/jwt/#delete-a-jwt-credential
+     */
     public function deleteCredential($consumer, $id)
     {
         return $this->response(
