863c121e35c8aff08652d9a8e478db0bf72aee02
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2021-07-04 10:45:15 +0800

    Completed Plugin: Proxy Cache

--- src/AdminAPI/Plugin/TrafficControl/ProxyCache.php
+++ src/AdminAPI/Plugin/TrafficControl/ProxyCache.php
@@ -13,6 +13,10 @@ class ProxyCache extends Plugin
         $this->purge();
     }
 
+    /**
+     * Retrieve a Cache Entity
+     * @link https://docs.konghq.com/hub/kong-inc/proxy-cache/#retrieve-a-cache-entity
+     */
     public function showByPlugin($plugin, $identifier)
     {
         return $this->response(
@@ -20,6 +24,35 @@ class ProxyCache extends Plugin
         );
     }
 
+    public function show($identifier)
+    {
+        return $this->response(
+            $this->client()->get($this->pluginPath().'/'.$identifier)
+        );
+    }
+
+    /**
+     * Delete Cache Entity
+     * @link https://docs.konghq.com/hub/kong-inc/proxy-cache/#delete-cache-entity
+     */
+    public function deletePluginCache($plugin, $identifier)
+    {
+        return $this->response(
+            $this->client()->delete($this->pluginPath() .'/'. $plugin.'/caches/'.$identifier);
+        );
+    }
+
+    public function deleteCache($identifier)
+    {
+        return $this->response(
+            $this->client()->delete($this->pluginPath() .'/'. $identifier)
+        );
+    }
+
+    /**
+     * Purge All Cache Entities
+     * @link https://docs.konghq.com/hub/kong-inc/proxy-cache/#purge-all-cache-entities
+     */
     public function purge()
     {
         return $this->response(
