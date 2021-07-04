3367b3bffd160d69d7a988dc8745004247cae282
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2021-07-04 12:28:17 +0800

    Apply PHP CS Fixer

--- src/AdminAPI/Plugin/Authentication/JWT.php
+++ src/AdminAPI/Plugin/Authentication/JWT.php
@@ -6,26 +6,26 @@ use KongGateway\AdminAPI\Plugin;
 
 class JWT extends Plugin
 {
-	public $name = 'jwt';
+    public $name = 'jwt';
 
-	public function createCredential($consumer)
-	{
-		return $this->response(
+    public function createCredential($consumer)
+    {
+        return $this->response(
             $this->client()->post($this->consumerPath($consumer).'/jwt')
         );
-	}
+    }
 
-	public function showConsumerCredentials($consumer)
-	{
-		return $this->response(
+    public function showConsumerCredentials($consumer)
+    {
+        return $this->response(
             $this->client()->get($this->consumerPath($consumer).'/jwt')
         );
-	}
+    }
 
-	public function deleteCredential($consumer, $id)
-	{
-		return $this->response(
-            $this->client()->delete($this->consumerPath($consumer).'/jwt/' . $id)
+    public function deleteCredential($consumer, $id)
+    {
+        return $this->response(
+            $this->client()->delete($this->consumerPath($consumer).'/jwt/'.$id)
         );
-	}
+    }
 }
